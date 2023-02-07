726bb5bb12669c11c02401911176276b33879d8b
StagRobotics1997 <124451810+StagRobotics1997@users.noreply.github.com>
2023-02-06 19:07:32 -0600

    code update

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,96 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+   // The robot's subsystems
+   private final DrivetrainSubsystem m_robotDrive = new DrivetrainSubsystem();
+   
+  
+   // Retained command handles
+
+   // The autonomous routines
+   // A simple auto routine that drives forward a specified distance, and then stops.
+   //private final Command m_simpleAuto = Autos.simpleAuto(m_robotDrive);
+   //private final Command m_complexAuto = Autos.complexAuto(m_robotDrive, m_shooter);
+   // A complex auto routine that drives forward, drops a hatch, and then drives backward.
+  
+   // A chooser for autonomous commands
+   SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+   // The driver's controller
+   //CommandPS4Controller m_driverController =
+   //    new CommandPS4Controller(OIConstants.kDriverControllerPort);
+   CommandJoystick m_primaryJoystick   = new CommandJoystick(OIConstants.PRIMARY_JOYSTICK);
+   CommandJoystick m_secondaryJoystick = new CommandJoystick(OIConstants.SECONDARY_JOYSTICK);
+   CommandJoystick m_auxJoystick       = new CommandJoystick(OIConstants.AUX_JOYSTICK);
+
+   /** The container for the robot. Contains subsystems, OI devices, and commands. */
+   public RobotContainer() {
+       // Configure the button bindings
+       configureButtonBindings();
+
+      // Configure default commands
+      // Set the default drive command to split-stick arcade drive
+      m_robotDrive.setDefaultCommand(
+      // A split-stick arcade command, with forward/backward controlled by the left
+      // hand, and turning controlled by the right.
+      Commands.run(
+            () ->
+                m_robotDrive.stickDrive(-m_primaryJoystick.getRawAxis(2), 
+                                         m_primaryJoystick.getRawAxis(1),
+                                         m_primaryJoystick.getRawAxis(3)),
+                m_robotDrive));
+
+      // Add commands to the autonomous command chooser
+      //m_chooser.setDefaultOption("Simple Auto", m_simpleAuto);
+      //m_chooser.addOption("Complex Auto", m_complexAuto);
+
+      // Put the chooser on the dashboard
+      Shuffleboard.getTab("Autonomous").add(m_chooser);
+   }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link PS4Controller}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+      m_primaryJoystick  
+           .button(7)
+           .onTrue(m_robotDrive.resetGyroscope());
+  }  
+
+//public CommandJoystick getPrimaryJoystick() {
+//  return m_primaryJoystick;
+//}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return m_chooser.getSelected();
+  }
+
+}
