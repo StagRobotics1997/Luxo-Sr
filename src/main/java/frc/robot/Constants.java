31cdd7c16b225554dd9bd6fd10061123067e11ed
Gary-Spotlight <77354735+Gary-Spotlight@users.noreply.github.com>
2023-02-08 08:25:11 -0600

    Added in motor/sensor prototype code.  some light formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,60 +9,66 @@ package frc.robot;
  * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public static final class DriveConstants {
-        public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 5;   // CAN
-        public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 6;   // CAN
+        public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 5; // CAN
+        public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 6; // CAN
         public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_ENCODER = 2; // Analog
-            
-        public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 1;   // CAN
-        public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 2;   // CAN
+
+        public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 1; // CAN
+        public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 2; // CAN
         public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_ENCODER = 0; // Analog
-                    
+
         public static final int DRIVETRAIN_BACK_LEFT_ANGLE_ENCODER = 3; // Analog
-        public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 8;   // CAN
-        public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 7;   // CAN
-            
-        public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 3;   // CAN
-        public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 4;   // CAN
+        public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 8; // CAN
+        public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 7; // CAN
+
+        public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 3; // CAN
+        public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 4; // CAN
         public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_ENCODER = 1; // Analog
     }
-        
+
     public static final class ArmConstants {
+        // Motor
         public static final int FOREARM_MOTOR = 0;
-         //Solenoids
+        // Solenoids
         public static final int BICEP_EXTENDER_1 = 1;
         public static final int BICEP_EXTENDER_2 = 2;
         public static final int WRIST_EXTENDER_1 = 3;
         public static final int WRIST_EXTENDER_2 = 4;
         public static final int FOREARM_EXTENDER_1 = 5;
-        public static final int FOREARM_EXTENDER_2 =6;
+        public static final int FOREARM_EXTENDER_2 = 6;
+        // Sensors
+        public static final int SENSOR_1 = 0;
+        public static final int SENSOR_2 = 1;
     }
-        //solenoids
+
     public static final class KickstandConstants {
+        // solenoids
         public static final int KICKER_EXTENDER_1 = 7;
         public static final int KICKER_EXTENDER_2 = 8;
     }
 
-        //Defibulator solenoids
-        public static final class DefibulatorConstants {
-            public static final int DEFIBULATOR_EXTENDER_1 = 7;
-            public static final int DEFIBULATOR_EXTENDER_2 = 8;
-        }
+    public static final class DefibulatorConstants {
+        // Solenoids
+        public static final int DEFIBULATOR_EXTENDER_1 = 7;
+        public static final int DEFIBULATOR_EXTENDER_2 = 8;
+    }
+
     public static final class ClawConstants {
         public static final int CLAW_MOTOR = 0;
 
-    public static final int CLAW_EXTENDER_1 = 3;
-    public static final int CLAW_EXTENDER_2 = 4;
+        public static final int CLAW_EXTENDER_1 = 3;
+        public static final int CLAW_EXTENDER_2 = 4;
     }
 
     public static final class OIConstants {
-        public static final int PRIMARY_JOYSTICK   = 0;
-	    public static final int SECONDARY_JOYSTICK = 1;
- 	    public static final int AUX_JOYSTICK       = 2;
+        public static final int PRIMARY_JOYSTICK = 0;
+        public static final int SECONDARY_JOYSTICK = 1;
+        public static final int AUX_JOYSTICK = 2;
     }
 }
-
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,103 +1,153 @@
 package frc.robot.subsystems;
+
 import frc.robot.Constants.ArmConstants;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
 public class ArmSubsystem extends SubsystemBase {
-    private DoubleSolenoid bicepExtender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, ArmConstants.BICEP_EXTENDER_1, ArmConstants.BICEP_EXTENDER_2);
-    private DoubleSolenoid forearmExtender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, ArmConstants.FOREARM_EXTENDER_1, ArmConstants.FOREARM_EXTENDER_2);
-    private DoubleSolenoid wristExtender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,ArmConstants.WRIST_EXTENDER_1, ArmConstants.WRIST_EXTENDER_2);
-    private boolean extendBicep = false;
-    private boolean extendWrist= false;
-    private boolean extendForearm= false;
-    private boolean onforearmmotor= false;
-    private Spark forearmMotor = new Spark(ArmConstants.FOREARM_MOTOR);
-    
-    public ArmSubsystem(){
-    }
-       
-  public void ToggleExtendBicep(){
-    if (extendBicep == false){
-        bicepExtender.set(DoubleSolenoid.Value.kReverse);
-        extendBicep = true;
-    }else if(extendBicep= true){
-        bicepExtender.set(DoubleSolenoid.Value.kForward);
-        extendBicep = false;
-    }
-  }  
-  
-  public void ToggleExtendFront(){
-    if (extendWrist == false){
-        wristExtender.set(DoubleSolenoid.Value.kReverse);
-        extendWrist = true;
-    }else if(extendWrist = true){
-       wristExtender.set(DoubleSolenoid.Value.kForward);
-        extendWrist = false;
-    }
-  } 
+  private DoubleSolenoid bicepExtender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, ArmConstants.BICEP_EXTENDER_1,
+      ArmConstants.BICEP_EXTENDER_2);
+  private DoubleSolenoid forearmExtender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,
+      ArmConstants.FOREARM_EXTENDER_1, ArmConstants.FOREARM_EXTENDER_2);
+  private DoubleSolenoid wristExtender = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, ArmConstants.WRIST_EXTENDER_1,
+      ArmConstants.WRIST_EXTENDER_2);
+  private boolean extendBicep = false;
+  private boolean extendWrist = false;
+  private boolean extendForearm = false;
+  private boolean onforearmmotor = false;
+  private final Spark forearmMotor = new Spark(ArmConstants.FOREARM_MOTOR);
+  private final DigitalInput m_sensor_1 = new DigitalInput(ArmConstants.SENSOR_1);
+  private final DigitalInput m_sensor_2 = new DigitalInput(ArmConstants.SENSOR_2);
+  private int currentPosition = 0;
 
-  public void ToggleExtendForearm(){
-    if (extendForearm == false){
-        forearmExtender.set(DoubleSolenoid.Value.kReverse);
-        extendForearm = true;
-    }else if(extendForearm = true){
-        forearmExtender.set(DoubleSolenoid.Value.kForward);
-        extendForearm = false;
+  public ArmSubsystem() {
+  }
+
+  public void ToggleExtendBicep() {
+    if (extendBicep == false) {
+      bicepExtender.set(DoubleSolenoid.Value.kReverse);
+      extendBicep = true;
+    } else if (extendBicep = true) {
+      bicepExtender.set(DoubleSolenoid.Value.kForward);
+      extendBicep = false;
     }
-  } 
-  public void toggleforearmMotor(double speed){
-    if (onforearmmotor == false){
+  }
+
+  public void ToggleExtendFront() {
+    if (extendWrist == false) {
+      wristExtender.set(DoubleSolenoid.Value.kReverse);
+      extendWrist = true;
+    } else if (extendWrist = true) {
+      wristExtender.set(DoubleSolenoid.Value.kForward);
+      extendWrist = false;
+    }
+  }
+
+  public void ToggleExtendForearm() {
+    if (extendForearm == false) {
+      forearmExtender.set(DoubleSolenoid.Value.kReverse);
+      extendForearm = true;
+    } else if (extendForearm = true) {
+      forearmExtender.set(DoubleSolenoid.Value.kForward);
+      extendForearm = false;
+    }
+  }
+
+  public void toggleforearmMotor(double speed) {
+    if (onforearmmotor == false) {
       forearmMotorForward();
       onforearmmotor = true;
-    }else{
+    } else {
       forearmMotorOff();
       onforearmmotor = false;
     }
   }
-     
+
   public void forearmMotorForward() {
     forearmMotor.set(0.5);
   }
-    
+
   public void forearmMotorBackward() {
-   forearmMotor.set(-0.5);
+    forearmMotor.set(-0.5);
   }
 
-        
-  public CommandBase forearmMotoron(){
+  public CommandBase forearmMotoron() {
     return this.runOnce(() -> forearmMotor.set(0.5));
-   }
- 
-        
-  public CommandBase forearmMotorOff(){
+  }
+
+  public CommandBase forearmMotorOff() {
     return this.runOnce(() -> forearmMotor.set(0.0));
   }
-      
- 
 
-  public CommandBase bicepIn(){
+  public CommandBase bicepIn() {
     return this.runOnce(() -> bicepExtender.set(DoubleSolenoid.Value.kReverse));
   }
 
-  public CommandBase bicepout(){
+  public CommandBase bicepout() {
     return this.runOnce(() -> bicepExtender.set(DoubleSolenoid.Value.kForward));
   }
 
-  public CommandBase foreArmIn(){
+  public CommandBase foreArmIn() {
     return this.runOnce(() -> forearmExtender.set(DoubleSolenoid.Value.kReverse));
   }
 
-  public CommandBase foreArmout(){
+  public CommandBase foreArmout() {
     return this.runOnce(() -> forearmExtender.set(DoubleSolenoid.Value.kForward));
   }
-  public CommandBase wristIn(){
+
+  public CommandBase wristIn() {
     return this.runOnce(() -> wristExtender.set(DoubleSolenoid.Value.kReverse));
   }
 
-  public CommandBase wristout(){
+  public CommandBase wristout() {
     return this.runOnce(() -> wristExtender.set(DoubleSolenoid.Value.kForward));
+  }
+
+  public boolean atDesiredPosition(int desiredPosition, boolean directionUp) {
+    int direction = 0;
+
+    if (directionUp)
+      direction = +1;
+    else
+      direction = -1;
+
+    if (currentPosition == 3 && directionUp)
+      return true;
+
+    if (currentPosition == 0 && !directionUp)
+      return true;
+
+    if (m_sensor_1.get()) {
+      currentPosition = currentPosition + direction;
+      if (currentPosition == desiredPosition) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void setMotorSpeed(double speed) {
+    if (speed > 0.0) {
+      if (!m_sensor_1.get()) {
+        forearmMotor.set(0.0); // Moving up and top limit is tripped so stop
+        SmartDashboard.putString("Lead Screw", "Off");
+      } else {
+        forearmMotor.set(speed); // Moving up, top limit is not tripped so continue at speed
+        SmartDashboard.putString("Lead Screw", "On");
+      }
+    } else {
+      if (!m_sensor_2.get()) {
+        forearmMotor.set(0.0); // Moving down and bottom limit is tripped so stop
+        SmartDashboard.putString("Lead Screw", "Off");
+      } else {
+        forearmMotor.set(speed); // Moving down, bottom limit is not tripped so continue at speed
+        SmartDashboard.putString("Lead Screw", "On");
+      }
+    }
   }
 }
