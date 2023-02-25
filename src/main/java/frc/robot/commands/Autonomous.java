3d6deb171a3fb11b02fe929311bc468d9c6b0e4c
bikerprincess <105526198+bikerprincess@users.noreply.github.com>
2023-02-25 09:19:41 -0600

    changed autonomous

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -4,18 +4,22 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj.Timer;
 
 public final class Autonomous {
 
     public static Command simpleCommand(DrivetrainSubsystem drive) {
         return Commands.sequence(
                 new StartEndCommand(() -> drive.stickDrive(.25, .0, .0),
-                        () -> drive.stickDrive(0.0, .0, .0), drive).withTimeout(3),
+                        () -> drive.stickDrive(0.0, .0, .0), drive),
+                        Commands.runOnce(()-> Timer.delay(3.0)),
                 new StartEndCommand(() -> drive.stickDrive(.0, .25, .0),
-                        () -> drive.stickDrive(0.0, .0, .0), drive).withTimeout(3),
+                        () -> drive.stickDrive(0.0, .0, .0), drive),
+                        Commands.runOnce(()-> Timer.delay(3.0)),
                 new StartEndCommand(() -> drive.stickDrive(-.25, .0, .0),
-                        () -> drive.stickDrive(0.0, .0, .0), drive).withTimeout(3),
+                        () -> drive.stickDrive(0.0, .0, .0), drive),
+                        Commands.runOnce(()-> Timer.delay(3.0)),
                 new StartEndCommand(() -> drive.stickDrive(.0, -.25, .0),
-                        () -> drive.stickDrive(0.0, .0, .0), drive).withTimeout(3));
+                        () -> drive.stickDrive(0.0, 0, .0), drive));
     }
 }
